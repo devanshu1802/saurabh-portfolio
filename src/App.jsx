084ea840f7709d6027f42c11286e9249773e629f
Devanshu <devanshusingh530@gmail.com>
2026-06-09 23:47:13 +0530

    chore: optimize bundle size, add SEO tags, and fix accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Blog from './pages/Blog'
-import Projects from './pages/Projects'
-import Agence from './pages/Agence'
 import Navbar from './components/Navigation/Navbar'
 import FullScreenNav from './components/Navigation/FullScreenNav'
 import Cursor from './components/common/Cursor'
 import Footer from './components/common/Footer'
 import PageTransition from './components/common/PageTransition'
 import SmoothScroll from './components/common/SmoothScroll'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+const Contact = lazy(() => import('./pages/Contact'))
+const Blog = lazy(() => import('./pages/Blog'))
+const Projects = lazy(() => import('./pages/Projects'))
+const Agence = lazy(() => import('./pages/Agence'))
 
 const App = () => {
   const location = useLocation();
@@ -24,16 +25,18 @@ const App = () => {
         <Navbar />
         <FullScreenNav />
         <div className="flex-grow">
-          <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-              <Route path='/' element={<PageTransition><Home /></PageTransition>} />
-              <Route path='/work' element={<PageTransition><Projects /></PageTransition>} />
-              <Route path='/agence' element={<PageTransition><Agence /></PageTransition>} />
-              <Route path='/about' element={<PageTransition><About /></PageTransition>} />
-              <Route path='/contact' element={<PageTransition><Contact /></PageTransition>} />
-              <Route path='/blog' element={<PageTransition><Blog /></PageTransition>} />
-            </Routes>
-          </AnimatePresence>
+          <Suspense fallback={<div className="min-h-screen flex items-center justify-center bg-black"><div className="w-10 h-10 border-4 border-[#defd50] border-t-transparent rounded-full animate-spin"></div></div>}>
+            <AnimatePresence mode="wait">
+              <Routes location={location} key={location.pathname}>
+                <Route path='/' element={<PageTransition><Home /></PageTransition>} />
+                <Route path='/work' element={<PageTransition><Projects /></PageTransition>} />
+                <Route path='/agence' element={<PageTransition><Agence /></PageTransition>} />
+                <Route path='/about' element={<PageTransition><About /></PageTransition>} />
+                <Route path='/contact' element={<PageTransition><Contact /></PageTransition>} />
+                <Route path='/blog' element={<PageTransition><Blog /></PageTransition>} />
+              </Routes>
+            </AnimatePresence>
+          </Suspense>
         </div>
         <Footer />
       </div>
--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -4,13 +4,13 @@ const ProjectCard = (props) => {
     return (
         <>
             <a href={props.link1 || "https://www.artstation.com/saurabh583"} target="_blank" rel="noreferrer" className='w-full h-1/2 lg:w-1/2 lg:h-full group transition-all relative rounded-none hover:rounded-[70px] overflow-hidden block'>
-                <img className='h-full w-full object-cover transition-transform duration-700 group-hover:scale-105' src={props.image1} alt="" />
+                <img className='h-full w-full object-cover transition-transform duration-700 group-hover:scale-105' src={props.image1} alt="Project preview" />
                 <div className='opacity-100 lg:opacity-0 transition-all duration-500 group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/20 lg:bg-black/40'>
                     <h2 className='uppercase lg:text-6xl md:text-5xl text-2xl font-[font1] border-2 md:border-4 px-4 py-1 lg:px-5 lg:pt-3 lg:pb-2 text-white rounded-full bg-black/40 lg:bg-transparent'>View Project</h2>
                 </div>
             </a>
             <a href={props.link2 || "https://www.artstation.com/saurabh583"} target="_blank" rel="noreferrer" className='w-full h-1/2 lg:w-1/2 lg:h-full group transition-all relative rounded-none hover:rounded-[70px] overflow-hidden block'>
-                <img className='h-full w-full object-cover transition-transform duration-700 group-hover:scale-105' src={props.image2} alt="" />
+                <img className='h-full w-full object-cover transition-transform duration-700 group-hover:scale-105' src={props.image2} alt="Project preview" />
                 <div className='opacity-100 lg:opacity-0 transition-all duration-500 group-hover:opacity-100 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/20 lg:bg-black/40'>
                     <h2 className='uppercase lg:text-6xl md:text-5xl text-2xl font-[font1] border-2 md:border-4 px-4 py-1 lg:px-5 lg:pt-3 lg:pb-2 text-white rounded-full bg-black/40 lg:bg-transparent'>View Project</h2>
                 </div>
