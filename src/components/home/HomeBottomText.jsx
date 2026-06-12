2ea1c58c7f19ec21f1cbfd3cf8a01567d1657039
Devanshu <devanshusingh530@gmail.com>
2026-06-12 19:50:17 +0530

    style: adjust font size for Projects and About Me links on large screens

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,12 +4,12 @@ const HomeBottomText = () => {
   return (
     <div className="font-[font2] flex flex-col md:flex-row items-center justify-center gap-2 w-full px-4">
       <div className="lg:border-3 border-2 w-full md:w-auto justify-center hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-25 flex items-center border-white px-3 py-2 lg:px-5 lg:pt-1.5 rounded-full uppercase">
-        <Link className="text-[6vw] lg:mt-4" to="/work">
+        <Link className="text-[6vw] lg:text-[4vw] lg:mt-4" to="/work">
           Projects
         </Link>
       </div>
       <div className="lg:border-3 border-2 w-full md:w-auto justify-center hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-25 flex items-center border-white px-3 py-2 lg:px-5 lg:pt-1.5 rounded-full uppercase">
-        <Link className="text-[6vw] lg:mt-4" to="/agence">
+        <Link className="text-[6vw] lg:text-[4vw] lg:mt-4" to="/agence">
           About Me
         </Link>
       </div>
